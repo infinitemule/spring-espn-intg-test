e14cdc3a810536cea0dfe46079a2099ee78de027
Dave Keller <keller@infinitemule.com>
2013-08-06 22:23:11 -0400

    Added integration test suite.

---
+++ src/test/java/com/infinitemule/espn/api/test/SpringEspnIntgTestSuite.java
@@ -0,0 +1,25 @@
+/*
+ * Spring ESPN
+ */
+package com.infinitemule.espn.api.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.infinitemule.espn.api.test.athletes.spring.AthletesApiServiceSpringIntgTest;
+import com.infinitemule.espn.api.test.now.spring.NowApiServiceSpringIntgTest;
+import com.infinitemule.espn.api.test.sports.spring.SportsApiServiceSpringIntgTest;
+import com.infinitemule.espn.api.test.teams.spring.TeamsApiServiceSpringIntgTest;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+  AthletesApiServiceSpringIntgTest.class,
+  NowApiServiceSpringIntgTest.class,
+  SportsApiServiceSpringIntgTest.class,
+  TeamsApiServiceSpringIntgTest.class
+})
+public class SpringEspnIntgTestSuite {
+
+}
