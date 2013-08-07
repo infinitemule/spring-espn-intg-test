e61958976d006a50b235610b16faa3bbfa67dfd7
Dave Keller <keller@infinitemule.com>
2013-08-06 22:30:26 -0400

    Added Headlines API test to integration suite.

--- src/test/java/com/infinitemule/espn/api/test/SpringEspnIntgTestSuite.java
+++ src/test/java/com/infinitemule/espn/api/test/SpringEspnIntgTestSuite.java
@@ -8,6 +8,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import com.infinitemule.espn.api.test.athletes.spring.AthletesApiServiceSpringIntgTest;
+import com.infinitemule.espn.api.test.headlines.spring.HeadlinesApiServiceSpringIntgTest;
 import com.infinitemule.espn.api.test.now.spring.NowApiServiceSpringIntgTest;
 import com.infinitemule.espn.api.test.sports.spring.SportsApiServiceSpringIntgTest;
 import com.infinitemule.espn.api.test.teams.spring.TeamsApiServiceSpringIntgTest;
@@ -16,6 +17,7 @@ import com.infinitemule.espn.api.test.teams.spring.TeamsApiServiceSpringIntgTest
 @RunWith(Suite.class)
 @SuiteClasses({ 
   AthletesApiServiceSpringIntgTest.class,
+  HeadlinesApiServiceSpringIntgTest.class,
   NowApiServiceSpringIntgTest.class,
   SportsApiServiceSpringIntgTest.class,
   TeamsApiServiceSpringIntgTest.class
