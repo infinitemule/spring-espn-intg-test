db01b710bf9765e0eced79a884835e40b1e20a7e
Dave Keller <keller@infinitemule.com>
2013-08-06 22:30:02 -0400

    Added test for Headlines API.

---
+++ src/test/java/com/infinitemule/espn/api/test/headlines/spring/HeadlinesApiServiceSpringIntgTest.java
@@ -0,0 +1,46 @@
+/*
+ * Spring ESPN
+ */
+package com.infinitemule.espn.api.test.headlines.spring;
+
+import static com.infinitemule.espn.common.lang.Console.printfn;
+import static com.infinitemule.espn.common.lang.Console.println;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.infinitemule.espn.api.headlines.Headline;
+import com.infinitemule.espn.api.headlines.HeadlinesApiRequest;
+import com.infinitemule.espn.api.headlines.HeadlinesApiResponse;
+import com.infinitemule.espn.api.headlines.spring.HeadlinesApiServiceSpring;
+import com.infinitemule.espn.api.test.AbstractApiServiceSpringIntgTest;
+
+public class HeadlinesApiServiceSpringIntgTest extends AbstractApiServiceSpringIntgTest {
+
+  @Autowired
+  private HeadlinesApiServiceSpring srv;
+
+    
+  @Test
+  public void newsAllCities() {
+    
+    HeadlinesApiRequest req = new HeadlinesApiRequest().news().forCities();
+        
+    output(srv.call(req));    
+  }
+  
+  
+  private void output(HeadlinesApiResponse response) {
+    
+    for(Headline headline : response.getHeadlines()) {
+      println("----------------------------------------");
+      printfn("%s (%s)", headline.getHeadline(), headline.getId());
+
+      println("  - Description:");
+      printfn("      %s - %s", headline.getPublished(), headline.getType());
+      printfn("      %s", headline.getTitle());
+      printfn("      %s", headline.getDescription());
+
+    }
+  }
+}
